26161640df992edff79c1552444ebfd5ecb636e9
Chip Thien <themacrochip@gmail.com>
2019-01-27 00:34:24 -0600

    Distant spark flickers

---
+++ Assets/Flickering.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("ChangeState", 5);
+    }
+
+    private void ChangeState()
+    {
+        gameObject.SetActive(!gameObject.active);
+        Invoke("ChangeState", Random.Range(1, 3));
+    }
+}
