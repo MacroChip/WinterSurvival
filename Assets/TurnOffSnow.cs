a05a8bab9776ffadb727a635d4eba5b3e81c73a0
Chip Thien <themacrochip@gmail.com>
2019-01-26 20:26:24 -0600

    Turn blizzard effects off when you enter your house. Turn them back on when you exit your house.

---
+++ Assets/TurnOffSnow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffSnow : MonoBehaviour
+{
+    private const string cabin = "WoodenCabinFbx";
+
+    public GameObject blizzard;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals(cabin))
+        {
+            blizzard.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name.Equals(cabin))
+        {
+            blizzard.SetActive(true);
+        }
+    }
+}
