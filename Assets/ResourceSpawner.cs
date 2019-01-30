80f85711eb95f2abb7ea6d4709b86d6786cdbd9b
Chip Thien <themacrochip@gmail.com>
2019-01-29 19:00:21 -0600

    Renamed WoodSpawner to ResourceSpawner. It can now spawn any object.

--- Assets/ResourceSpawner.cs
+++ Assets/ResourceSpawner.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WoodSpawner : MonoBehaviour
+public class ResourceSpawner : MonoBehaviour
 {
-    public GameObject woodToSpawn;
+    public GameObject resourceToSpawn;
     private TerrainData terrainData;
-    public int spawnRate = 30;
+    public int spawnRate;
 
     void Start()
     {
         GameObject terrainObject = GameObject.Find("Terrain");
         terrainData = terrainObject.GetComponent<Terrain>().terrainData;
-        InvokeRepeating("SpawnWood", 0f, spawnRate);
+        InvokeRepeating("SpawnResource", 0f, spawnRate);
     }
 
-    void SpawnWood()
+    void SpawnResource()
     {
         Vector3 position = new Vector3(Random.Range(0f, terrainData.size.x), 0.1f, Random.Range(0f, terrainData.size.z));
-        Debug.Log("spawning wood at " + position.ToString());
-        Instantiate(woodToSpawn, position, Quaternion.identity);
+        Debug.Log($"spawning {resourceToSpawn.name} at " + position.ToString());
+        Instantiate(resourceToSpawn, position, Quaternion.identity);
     }
 }
