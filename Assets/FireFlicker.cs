dfa06bb509787242947ab4768dede7bef02c05e3
Chip Thien <themacrochip@gmail.com>
2019-02-03 00:31:13 -0600

    Use better fire flicker script. It can transition intensity more sharply like a chaotic fire.

---
+++ Assets/FireFlicker.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class FireFlicker : MonoBehaviour
+{
+    public float minIntensity;
+    public float maxIntensity;
+
+    [Tooltip("How much to smooth out the randomness; lower values = sparks, higher = lantern")]
+    [Range(1, 50)]
+    public int smoothing = 5;
+
+    // Continuous average calculation via FIFO queue
+    // Saves us iterating every time we update, we just change by the delta
+    Queue<float> smoothQueue;
+    float lastSum = 0;
+
+
+    /// <summary>
+    /// Reset the randomness and start again. You usually don't need to call
+    /// this, deactivating/reactivating is usually fine but if you want a strict
+    /// restart you can do.
+    /// </summary>
+    public void Reset()
+    {
+        smoothQueue.Clear();
+        lastSum = 0;
+    }
+
+    void Start()
+    {
+        smoothQueue = new Queue<float>(smoothing);
+    }
+
+    void Update()
+    {
+        while (smoothQueue.Count >= smoothing)
+        {
+            lastSum -= smoothQueue.Dequeue();
+        }
+
+        float newVal = Random.Range(minIntensity, maxIntensity);
+        smoothQueue.Enqueue(newVal);
+        lastSum += newVal;
+
+        GetComponent<Light>().intensity = lastSum / (float)smoothQueue.Count;
+    }
+}
