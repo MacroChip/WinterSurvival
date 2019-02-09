0740f1a172c7f89d844a6f25abafae7414c158fe
Chip Thien <themacrochip@gmail.com>
2019-02-08 19:09:06 -0600

    Wood needs to be in the stove to be burned. Deprecating global inventory.

--- Assets/WoodBurning.cs
+++ Assets/WoodBurning.cs
@@ -11,6 +11,7 @@ public class WoodBurning : MonoBehaviour
     public AudioSource fireSound;
     public Light fireLight;
     private bool isLit;
+    public GameObject wood;
 
     void Start()
     {
@@ -21,7 +22,8 @@ public class WoodBurning : MonoBehaviour
 
     void ConsumeWood()
     {
-        if (globalInventory.wood - numberOfWoodBurnedPerTick < 0)
+        Collider woodInsideStove = GetWoodThatIsInsideStove();
+        if (!woodInsideStove)
         {
             Debug.Log("Tried to burn wood, but you don't have enough");
             ExtinguishFire();
@@ -32,8 +34,22 @@ public class WoodBurning : MonoBehaviour
                 Debug.Log("Igniting fire");
                 LightMyFire();
             }
-            globalInventory.DecrementWood(numberOfWoodBurnedPerTick);
+            GameObject.Destroy(woodInsideStove.gameObject);
         }
+    }
+
+    private Collider GetWoodThatIsInsideStove()
+    {
+        BoxCollider stoveCollider = GetComponent<BoxCollider>();
+        Vector3 size = stoveCollider.size;
+        Collider[] collisions = Physics.OverlapBox(gameObject.transform.position, size / 2);
+        foreach (var collider in collisions)
+        {
+            if (collider.name == wood.name + "(Clone)") {
+                return collider;
+            }
+        }
+        return null;
     }
 
     private void LightMyFire()
