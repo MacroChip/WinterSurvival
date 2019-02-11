e48f04f2a55ac0a4ceb224c16cc1d8bddc527e2f
Chip Thien <themacrochip@gmail.com>
2019-02-10 20:22:35 -0600

    Added fire starter. You start with it in your hands

---
+++ Assets/HeldItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeldItem : MonoBehaviour
+{
+    public Transform startingItem;
+
+    void Start()
+    {
+        Transform startingItemHeld = Instantiate(startingItem, Camera.main.transform.position - new Vector3(0, 0.186f, 1.049f), Quaternion.Euler(90, 20, 0), Camera.main.transform);
+        startingItemHeld.localScale = new Vector3(3, 3, 3);
+    }
+}
