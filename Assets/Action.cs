59104f8af729b1ef37875116433ce6b3dca3d9fd
Chip Thien <themacrochip@gmail.com>
2019-02-12 04:28:51 -0600

    Added shot limiter. It's currently hard coded to the fire starter. lastFiredTime is initialized so that you can fire immediately.

--- Assets/Action.cs
+++ Assets/Action.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Action : MonoBehaviour
 {
+    private const float FIRE_STARTER_SECONDS_BETWEEN_SHOTS = 2;
     public Transform actionAnimation;
+
+    private float lastFiredTime = -FIRE_STARTER_SECONDS_BETWEEN_SHOTS;
 
     void Update()
     {
-        if(Input.GetButton("Fire1"))
+        if(Input.GetButton("Fire1") && Time.time - lastFiredTime >= FIRE_STARTER_SECONDS_BETWEEN_SHOTS)
         {
+            lastFiredTime = Time.time;
             Transform animation = Instantiate(actionAnimation, gameObject.transform);
             animation.transform.rotation *= Quaternion.Euler(90, 0, 0);
         }
