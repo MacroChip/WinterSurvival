39bca56c1a6dd07fa8590a12ece0fd4d8cfa714b
Chip Thien <themacrochip@gmail.com>
2019-02-12 00:55:43 -0600

    Pull in spark effect from unity assets. On click, fire starter sparks. Sparks don't stop and have no shot limiter.

---
+++ Assets/Action.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour
+{
+    public Transform actionAnimation;
+
+    void Update()
+    {
+        if(Input.GetButton("Fire1"))
+        {
+            Transform animation = Instantiate(actionAnimation, gameObject.transform);
+            animation.transform.rotation *= Quaternion.Euler(90, 0, 0);
+        }
+    }
+}
